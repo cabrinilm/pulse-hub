791528ace24ed58b45cc03acc9a0d1c817f66b3f
cabrinilm <luca_cabrinimm@hotmail.com>
2025-10-01 22:16:57 +0100

    token available

---
+++ backend/__tests__/profile.test.js
@@ -0,0 +1,20 @@
+import request from 'supertest';
+require('dotenv').config()
+import { supabase } from '../src/services/supabaseClient.ts';
+import { beforeEach } from 'node:test';
+
+describe('GET /profile ', () => {
+
+
+  beforeEach(async () => {
+
+
+    
+
+  })
+
+
+
+
+
+})
