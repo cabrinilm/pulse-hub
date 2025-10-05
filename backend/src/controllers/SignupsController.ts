a2312ac36e971953429e3210e555974cd44583ba
cabrinilm <luca_cabrinimm@hotmail.com>
2025-10-06 00:54:21 +0100

    SignupsController.signupevent done

---
+++ backend/src/controllers/SignupsController.ts
@@ -0,0 +1,52 @@
+import type { Request, Response } from "express";
+import SignupsModel from "../models/SignupsModel";
+
+class SignupsController {
+  // signup event
+
+  async SignupEvent(req: Request, res: Response): Promise<void> {
+    try {
+      const eventId = req.params.id;
+      const supabase = req.supabase;
+      const user_id = req.user?.id;
+      const { payment_status, presence_status } = req.body;
+
+      if (!eventId) {
+        res.status(400).json({ error: "Event ID is required" });
+        return;
+      }
+      if (!supabase) {
+        res.status(500).json({ error: "Supabase client not found in request" });
+        return;
+      }
+      if (!user_id) {
+        res.status(401).json({ error: "Unauthorized: No user ID found" });
+        return;
+      }
+
+      const sucess = await SignupsModel.signupEvent(supabase, user_id, {
+        event_id: eventId,
+        payment_status,
+        presence_status,
+      });
+
+      if (!sucess) {
+        res.status(404).json({ error: "Event creation failed" });
+        return;
+      }
+
+      res.status(201).json(sucess);
+    } catch (error) {
+      if (error instanceof Error && error.message.includes("Forbidden")) {
+        res.status(403).json({ error: error.message });
+        return;
+      }
+      res.status(500).json({
+        error: error instanceof Error ? error.message : "Unknown error",
+      });
+    }
+  }
+}
+
+const signupsController = new SignupsController();
+export default signupsController;
